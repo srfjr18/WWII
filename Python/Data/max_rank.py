c4a4bde5b96bac6764202c4f515ba44ec6df0003
srfjr18 <srfjr18@gmail.com>
2017-12-23 23:16:17 -0500

    update

--- Python/Data/max_rank.py
+++ Python/Data/max_rank.py
@@ -2,10 +2,10 @@
 import pickle, os
 
 path = os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)), 'userdata')
-with open(path, "r") as file:
+with open(path, "rb") as file:
     data = pickle.load(file)
                     
 data["rank"] = 834
 
-with open(path, "w+") as file:
+with open(path, "wb+") as file:
     pickle.dump(data, file, protocol=2)
