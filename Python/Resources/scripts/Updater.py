84307ca6869dc222c6a597f050fd350a25c3e004
srfjr18 <srfjr18@gmail.com>
2018-02-25 12:23:23 -0500

    update

--- Python/Resources/scripts/Updater.py
+++ Python/Resources/scripts/Updater.py
@@ -4,7 +4,6 @@ from zipfile import *
 def update(build):
     files = os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'file.zip')
     path = os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), '')
-    path = os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-3]), '')
     urllib.urlretrieve('https://github.com/srfjr18/WWII-base/archive/master.zip', files)
     zip = ZipFile(files)
     zip.extractall(path)
@@ -14,15 +13,10 @@ def update(build):
             if build in check.read()
                 return "up to date"
     
-    if os.path.isdir(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-3]), 'Python'): #using embedded zip
-        
-        shutil.rmtree(path+'Resources')
-        shutil.copytree(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master', 'Resources') path)
-        shutil.copy2(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master', 'game.py') back3)
-    else:
-        shutil.rmtree(path+'Resources')
-        shutil.copytree(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master', 'Resources') path)
-        shutil.copy2(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master', 'game.py') path)
-        
+
+    shutil.rmtree(path+'Resources')
+    shutil.copytree(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master', 'Resources') path)
+    shutil.copy2(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master', 'game.py') path)
+    
     shutil.rmtree(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'WWII-base-master'))
     os.remove(os.path.join(os.path.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.path.sep)[:-2]), 'file.zip'))
--- Python/WWII-base-master/README.md
+++
@@ -1,2 +0,0 @@
-# WWII-base
-WWII with just the game files, no embedded python 3
